211cfb36dd7125bb27496c07a68f913b0fd9b0b4
eneilson lima oliveira <eneilson6@gmail.com>
2023-11-09 17:27:33 -0300

    crete sadebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { Header } from './components/Header';
+import { Post } from './Post'
 
-import reactLogo from './assets/react.svg'
+import styles from './App.module.css';
 
-
+import './global.css';
+import { Sadebar } from './components/Sadebar';
 
 export function App() {
-
-
   return (
-    <>
-      <div>Hellow World</div>
-        
-    </>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sadebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
+    </div>
   )
 }
-
---
+++ src/components/Sadebar.jsx
@@ -0,0 +1,24 @@
+import styles from './Sadebar.module.css';
+
+export function Sadebar() {
+  return (
+    <aside className={styles.sadebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/eneilson-oliveira.png" />
+        <strong>Eneilson</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
